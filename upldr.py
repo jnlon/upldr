66da664e77eebf15d7a25c84059eb9e9c99785b3
spinclick <spinclick@users.noreply.github.com>
2017-12-12 17:12:05 +0000

    Use REQUEST_URI instead of hardcoding /files/

--- upldr.py
+++ upldr.py
@@ -2,29 +2,31 @@
 
 import cgi
 import os
+from urllib.parse import urlsplit
 
 def upload_form_html():
   return """
-  <form action="/files/" method="post" enctype="multipart/form-data" >
+  <form action="{0}" method="post" enctype="multipart/form-data" >
     <input type="file" id="file" name="file">
     <input type="submit" value="Upload" name="submit">
-  </form> """
+  </form> """.format(os.getenv("REQUEST_URI"))
 
 def home_page_link_html():
-  return '<p><a href="/files/">Go back</a></p>'
+  endpoint = os.getenv("REQUEST_URI")
+  return '<p><a href="{0}">Go back</a></p>'.format(urlsplit(endpoint).path)
 
 def files_html():
-
   files = os.listdir('store/')
+  endpoint = os.getenv("REQUEST_URI")
   fmt = """
   <li> 
-    <form action="/files/" style="display:inline;">
-      <button type="submit" name="delete" value="{0}">delete</button>
+    <form action="{0}" style="display:inline;">
+      <button type="submit" name="delete" value="{1}">delete</button>
     </form>
-    <a href="store/{0}">{0}</a> 
+    <a href="store/{1}">{1}</a> 
   </li>
   """
-  files_list_html = '\n'.join([fmt.format(file) for file in files])
+  files_list_html = '\n'.join([fmt.format(endpoint, file) for file in files])
 
   return """
     <h2> Files ({}) </h2>
@@ -52,22 +54,30 @@ def get(form):
 
 def post(form):
   filename = form['file'].filename
+
   if filename == '':
     print("No file submitted")
     print(home_page_link_html())
     return
 
   outname = os.path.join("store", os.path.basename(filename))
-  print('Saving to {}...<br>'.format(outname))
+  print('Uploading ...<br>'.format(outname))
+
   with open(outname, 'wb') as outfile:
     outfile.write(form['file'].value)
-  print('Done writing {} bytes\n'.format(len(form['file'].value)))
+
+  print('Saved {0} bytes to <a href="{1}">{1}</a> \n'.format(
+    len(form['file'].value),
+    outname
+  ))
+
   print(home_page_link_html())
 
 def main():
   form = cgi.FieldStorage()
   print('Content-Type: text/html\n')
-  method = os.getenv("REQUEST_METHOD")
+
+  method = os.getenv('REQUEST_METHOD')
 
   os.makedirs("store", exist_ok=True)
 
